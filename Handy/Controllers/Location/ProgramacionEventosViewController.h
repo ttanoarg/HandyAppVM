8694e94463ef705f2f97d7cc025bf757913dab97
ttanoarg <Planetmedia@MacBook-Pro-de-Fernando.local>
2013-11-05 00:15:59 +0100

    Actualización eventos

---
+++ Handy/Controllers/Location/ProgramacionEventosViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProgramacionEventosViewController.h
+//  Handy
+//
+//  Created by Fernando Pabón on 05/11/13.
+//  Copyright (c) 2013 VM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgramacionEventosViewController : UIViewController
+
+@end
